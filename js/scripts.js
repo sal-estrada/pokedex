4d336795c7739d358ccb0a22ad3a24c0b3de67c7
Sal Estrada <sal.estrada21@gmail.com>
2025-06-11 12:05:45 -0400

    Updated JS
    Updated JS file include complex data types of starter Pokemon from generation 1.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,3 @@
-alert('Hello world');
-
-let favoriteFood = 'Poke';
-
-document.write(favoriteFood);
+pokemonList = [{name: 'bulbasaur', height: 7, type: ['grass', 'poison']},
+    {name: 'squirtle', height: 0.5, type: water},
+    {name: 'charmander', height: 0.6, type: fire}];
