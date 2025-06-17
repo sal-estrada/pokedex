8a2bb0751915da18697706ef9607681e7cd34914
Sal Estrada <sal.estrada21@gmail.com>
2025-06-17 10:45:53 -0400

    Refactored Code

--- js/scripts.js
+++ js/scripts.js
@@ -1,35 +1,23 @@
-// This code is for the content to be displayed according to Part 1.
-// pokemonList = [
-//     {name: 'Bulbasaur', height: 7, type: ['grass', ' poison']},
-//     {name: 'Squirtle', height: 0.5, type: 'water'},
-//     {name: 'Charmander', height: 0.6, type: 'fire'}
-// ];
 
-// for (let i = 0; i < pokemonList.length; i++) {
-//    let name = pokemonList[i].name;
-//    let height = pokemonList[i].height;
-//    let type = pokemonList[i].type
-
-//    if (Array.isArray(type)) {
-//     type = type.join(', ');
-// };
-//     document.write(name + " (height: " + height + ") - Type: " + type + "<br>");
-// }
-
-// This code is for the content to be displayed according to Part 3.
 let pokemonList = [
     {name: 'Bulbasaur', height: 7, type: ['grass', ' poison']},
     {name: 'Squirtle', height: 0.5, type: 'water'},
     {name: 'Charmander', height: 0.6, type: 'fire'}
   ];
   
-  for (let i = 0; i < pokemonList.length; i++) {
-    let name = pokemonList[i].name;
-    let height = pokemonList[i].height;
+  // for (let i = 0; i < pokemonList.length; i++) {
+  //   let name = pokemonList[i].name;
+  //   let height = pokemonList[i].height;
   
-    if (height > 1.0) {
-      document.write(name + " (height: " + height + ") - " + "Wow! You are a big Pokemon!<br>");
-    } else {
-      document.write(name + " (height: " + height + ")<br>");
-    }
-  }
+  //   if (height > 1.0) {
+  //     document.write(name + " (height: " + height + ") - " + "Wow! You are a big Pokemon!<br>");
+  //   } else {
+  //     document.write(name + " (height: " + height + ")<br>");
+  //   }
+  // }
+
+  // REFACTORED CODE
+
+  pokemonList.forEach(function(pokemon) {
+    console.log(pokemon.name + ", height: " + pokemon.height);
+  });
