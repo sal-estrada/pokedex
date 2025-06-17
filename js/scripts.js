4debaa8f20f63faf5d9bec2589ce074fd6f71e29
Sal Estrada <sal.estrada21@gmail.com>
2025-06-17 11:30:33 -0400

    Add IIFE Function

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,9 @@
 
-let pokemonList = [
-    {name: 'Bulbasaur', height: 7, type: ['grass', ' poison']},
-    {name: 'Squirtle', height: 0.5, type: 'water'},
-    {name: 'Charmander', height: 0.6, type: 'fire'}
-  ];
+// let pokemonList = [
+//     {name: 'Bulbasaur', height: 7, type: ['grass', ' poison']},
+//     {name: 'Squirtle', height: 0.5, type: 'water'},
+//     {name: 'Charmander', height: 0.6, type: 'fire'}
+//   ];
   
   // for (let i = 0; i < pokemonList.length; i++) {
   //   let name = pokemonList[i].name;
@@ -18,6 +18,38 @@ let pokemonList = [
 
   // REFACTORED CODE
 
-  pokemonList.forEach(function(pokemon) {
-    console.log(pokemon.name + ", height: " + pokemon.height);
-  });
+let pokemonRepository = (function() {
+  let pokemonList = [
+    {name: 'Bulbasaur', height: 7, type: ['grass', ' poison']},
+    {name: 'Squirtle', height: 0.5, type: 'water'},
+    {name: 'Charmander', height: 0.6, type: 'fire'}
+  ]
+
+  function getAll () {
+    return pokemonList
+  }
+
+  function add (pokemon) {
+    if (typeof pokemon === 'object' && 
+    pokemon !== null &&
+    typeof pokemon.name === 'string' &&
+    typeof pokemon.height === 'number' &&
+    pokemon.type !== undefined
+  ) {
+    pokemonList.push(pokemon);
+  } else console.log("Wrong input! Please use the correct format.")
+}
+
+  return {
+    getAll: getAll,
+    add: add
+  }
+})()
+
+pokemonRepository.getAll().forEach(function(pokemon) {
+  console.log(pokemon.name + ", height: " + pokemon.height);
+});
+
+pokemonRepository.add({name:'Pikachu', height: 3, type: 'electric'});
+
+console.log(pokemonRepository.getAll())
