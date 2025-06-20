7e2ab7aa924d3fa049bdfdc3a76cfde4604e9327
Sal Estrada <sal.estrada21@gmail.com>
2025-06-19 22:31:09 -0400

    Add Functions

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,5 @@
 
-// let pokemonList = [
-//     {name: 'Bulbasaur', height: 7, type: ['grass', ' poison']},
-//     {name: 'Squirtle', height: 0.5, type: 'water'},
-//     {name: 'Charmander', height: 0.6, type: 'fire'}
-//   ];
-  
-  // for (let i = 0; i < pokemonList.length; i++) {
-  //   let name = pokemonList[i].name;
-  //   let height = pokemonList[i].height;
-  
-  //   if (height > 1.0) {
-  //     document.write(name + " (height: " + height + ") - " + "Wow! You are a big Pokemon!<br>");
-  //   } else {
-  //     document.write(name + " (height: " + height + ")<br>");
-  //   }
-  // }
-
-  // REFACTORED CODE
+// Using DOM Practice
 
 let pokemonRepository = (function() {
   let pokemonList = [
@@ -38,18 +21,36 @@ let pokemonRepository = (function() {
   ) {
     pokemonList.push(pokemon);
   } else console.log("Wrong input! Please use the correct format.")
+  }
+
+function addListItem(pokemon){
+  let element = document.querySelector('.pokemon-list');
+  let listItem = document.createElement('li');
+  let button = document.createElement('button');
+  button.innerText = pokemon.name;
+  button.classList.add("button");
+  listItem.appendChild(button);
+  element.appendChild(listItem);
+  button.addEventListener('click', function (){
+  showDetails(pokemon)});
 }
+
+function showDetails(pokemon){
+  console.log(pokemon);
+} 
 
   return {
     getAll: getAll,
-    add: add
+    add: add,
+    addListItem: addListItem
+
   }
 })()
 
+pokemonRepository.add({name: 'Pikachu', height: 3.1, type: 'Electric'});
+
 pokemonRepository.getAll().forEach(function(pokemon) {
-  console.log(pokemon.name + ", height: " + pokemon.height);
-});
+  pokemonRepository.addListItem(pokemon);   
+  }
+);
 
-pokemonRepository.add({name:'Pikachu', height: 3, type: 'electric'});
-
-console.log(pokemonRepository.getAll())
